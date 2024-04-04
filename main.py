051e6665eda10448ab9dfbfb521bf97a385efa2c
Muhammad Rizwan <entitythreads@gmail.com>
2024-04-04 20:58:36 +0500

    :hammer: Updated main entrypoint

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# Importing FastAPI packages
 from fastapi import FastAPI
 
-app = FastAPI()
+
+app = FastAPI(
+    docs_url="/docs",
+    redoc_url="/redoc",
+    title="Entity Inference APIs",
+    description="Entity Inference APIs using AI models",
+    version="0.1a",
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+)
 
 
-@app.get("/")
+# --------------------------------------------------------------------------- #
+
+
+@app.get("/", include_in_schema=False)
 async def read_root():
     return {"message": "Entity Inference APIs application is up and running!"}
